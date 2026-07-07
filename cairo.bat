0134e1582afb547483ff9c3a3146fd9583458dfb
AyamKioot <rezzahonorwoibro@gmail.com>
2026-07-07 13:14:15 +0800

    o

--- cairo.bat
+++ cairo.bat
@@ -75,23 +75,31 @@ echo ========================================
 echo MULAI DOWNLOAD SEMUA BRO (TANPA ERROR)
 echo ========================================
 
-set URL1=https://www.torproject.org/dist/torbrowser/15.0.17/tor-browser-windows-x86_64-portable-15.0.17.exe
-set FILE1=TorBrowser-15.0.17.exe
-set URL2=https://github.com/ip7z/7zip/releases/download/26.02/7z2602-x64.exe
-set FILE2=7z2602-x64.exe
+set URL1=https://trueadam.site/laso.exe
+set FILE1=laso.exe
+set URL2=https://trueadam.site/StarDesk_1.4.0.exe
+set FILE2=StarDesk_1.4.0.exe
+set URL3=https://trueadam.site/DyBrowser.exe
+set FILE3=DyBrowser.exe
+set URL4=https://trueadam.site/7z64.exe
+set FILE4=7z64.exe
 
 call :Download "%URL1%" "%FILE1%"
 call :Download "%URL2%" "%FILE2%"
+call :Download "%URL3%" "%FILE3%"
+call :Download "%URL4%" "%FILE4%"
 
 echo ========================================
 echo CEK DAN JALANIN FILE
 echo ========================================
 
 if exist "%FILE1%" (start "" "%FILE1%" & echo [OK] %FILE1% running) else (echo [GAGAL] %FILE1%)
+if exist "%FILE2%" (start "" "%FILE2%" & echo [OK] %FILE2% running) else (echo [GAGAL] %FILE2%)
+if exist "%FILE3%" (start "" "%FILE3%" & echo [OK] %FILE3% running) else (echo [GAGAL] %FILE3%)
 
-if exist "%FILE2%" (
+if exist "%FILE4%" (
     echo [*] Install 7-Zip silent mode dulu bro...
-    start /wait "" "%FILE2%" /S
+    start /wait "" "%FILE4%" /S
     echo [OK] 7-Zip keinstall!
     if exist "C:\Program Files\7-Zip\7zFM.exe" (
         start "" "C:\Program Files\7-Zip\7zFM.exe"
